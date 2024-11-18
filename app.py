a5d451bd86f6410f611e1dda475658bf10fc048d
Reina4334 <157434948+reinaj28@users.noreply.github.com>
2024-11-18 09:00:48 -0800

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,14 @@ import os, os.path
 import shutil
 import pathlib
 import streamlit as st
-from simplified_rag.RAG import generate_answer
+#from simplified_rag.RAG import generate_answer 
+from UI-Retips import get_embeddings.py
+from UI-Retips import create_prompt.py
+from UI-Retips import query_for_results.py
 
-st.title("RAG Model Benefit Demonstration")
-st.header("\n A Simplified Approach")
+
+st.title("Hospital Incident Reports")
+st.header("AI summarizer")
 
 if 'selection' not in st.session_state:
     st.session_state.selection = ""
@@ -20,59 +24,35 @@ target_folder.mkdir(parents=True, exist_ok=True)
 # activating selection box for user choice 
 options = False
 emp = st.empty()
-vari = emp.selectbox(
-    key = "Options",
-    label = "Please select the option for query running:",
-    options = ("Wikipedia", "Research Paper")
+vari = "Hospital Reports"
 )
 
 if st.button("Select"):
     choice = vari
     options = True
 
-
-def wiki_choice() -> str:
-    wiki_query = st.text_input(
-        label = "Please input your Wikipedia search query",
-        max_chars = 256
-    )
-
-    if st.button("Submit"):
-        return wiki_query
-
-
 def research_choice() -> str:
     
     with st.form(key="doc_upload", clear_on_submit=False):
 
-        uploaded_doc = st.file_uploader(
-            label="Please upload your document",
-            accept_multiple_files = False,
-            type=['pdf']
+        doc_key = st.text_innput(
+            label="Please detail specific columns or key to answer query",
+            max_chars = 256
         )
+        
         research_query = st.text_input(
-            label = "Please input what you want to search",
+            label = "Enter Question",
             max_chars = 256
         )
         submit_button1 = st.form_submit_button("Load Document")
 
-    if submit_button1:
+    if submit_button1: #We dont have a file to upload just a key to look through every file
         with open(os.path.join(target_folder, uploaded_doc.name), 'wb') as f:
             f.write(uploaded_doc.getbuffer())
         return research_query
 
 
 def main(selection):
-    if selection == "Wikipedia":
-        wiki_query = wiki_choice()
-
-        if wiki_query is not None:
-            with st.spinner("Processing your request..."):
-                answer = generate_answer(selection, wiki_query)
-
-                st.success("Data processing complete!")
-                st.markdown(f"###### {answer['result']}")
-    elif selection == "Research Paper":
         research_query = research_choice()
 
         if research_query is not None:
